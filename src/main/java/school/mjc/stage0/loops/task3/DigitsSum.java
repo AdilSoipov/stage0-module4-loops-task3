62e744a51e1205d7dbb28e3da1ff0f9de414b3a0
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-20 21:50:14 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        
+        
+        int r, sum=0;
+        
+        while(t>0) {
+            
+            r = t % 10;
+            sum = sum + r;
+            t = t / 10;
+                        
+        }
+        
+        System.out.println(sum);
+        
+        
 
     }
 }
